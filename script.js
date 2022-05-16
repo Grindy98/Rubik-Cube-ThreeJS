8998c4a888edaa7919d6322e6bafafaca75cdb35
Stefan Malanik <stefanmalanik@gmail.com>
2022-05-16 23:28:32 +0300

    V3

--- script.js
+++ script.js
@@ -56,161 +56,53 @@ statics.rotateX( - Math.PI / 2 );
 
 scene.add(statics)
 
-
-function create_cube(size, materials){
-    // faces are front back up down left right
-    size *= 0.6
-    const geo = new THREE.BoxGeometry(size, size, size);
-    const cube = new THREE.Group();
-    const mesh = new THREE.Mesh( geo, materials );
-    const edges = new THREE.EdgesGeometry( geo );
-    const line = new THREE.LineSegments( edges,  lineMat);
-    cube.add( line );
-    cube.add( mesh );
-    return cube
-}
-
 class Rubik{
-    constructor(size){
+    constructor(size, scale){
         this.size = size
+        this.scale = scale
         this.pivot_mesh = new THREE.Group();
         this.rubik_mesh = new THREE.Group();
     
-        const cube_list = [];
         this.center_cubes = {};
+        this.list = []
 
         // Right
-        var cube = create_cube(size, [K, G, K, Y, K, O]);
-        cube.position.copy(new THREE.Vector3(0, 0, 0).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [K, K, K, Y, K, O]);
-        cube.position.copy(new THREE.Vector3(1, 0, 0).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [B, K, K, Y, K, O]);
-        cube.position.copy(new THREE.Vector3(2, 0, 0).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-
-        var cube = create_cube(size, [K, G, K, K, K, O]);
-        cube.position.copy(new THREE.Vector3(0, 1, 0).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [K, K, K, K, K, O]);
-        cube.position.copy(new THREE.Vector3(1, 1, 0).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.center_cubes.O = cube;
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [B, K, K, K, K, O]);
-        cube.position.copy(new THREE.Vector3(2, 1, 0).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-
-        var cube = create_cube(size, [K, G, W, K, K, O]);
-        cube.position.copy(new THREE.Vector3(0, 2, 0).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [K, K, W, K, K, O]);
-        cube.position.copy(new THREE.Vector3(1, 2, 0).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [B, K, W, K, K, O]);
-        cube.position.copy(new THREE.Vector3(2, 2, 0).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-
+        this.insertCube(0, 0, 0, [K, G, K, Y, K, O]);
+        this.insertCube(1, 0, 0, [K, K, K, Y, K, O]);
+        this.insertCube(2, 0, 0, [B, K, K, Y, K, O]);
+        this.insertCube(0, 1, 0, [K, G, K, K, K, O]);
+        this.insertCube(1, 1, 0, [K, K, K, K, K, O], 'O');
+        this.insertCube(2, 1, 0, [B, K, K, K, K, O]);
+        this.insertCube(0, 2, 0, [K, G, W, K, K, O]);
+        this.insertCube(1, 2, 0, [K, K, W, K, K, O]);
+        this.insertCube(2, 2, 0, [B, K, W, K, K, O]);
 
         // Middle
-        var cube = create_cube(size, [K, G, K, Y, K, K]);
-        cube.position.copy(new THREE.Vector3(0, 0, 1).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [K, K, K, Y, K, K]);
-        cube.position.copy(new THREE.Vector3(1, 0, 1).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.center_cubes.Y = cube;
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [B, K, K, Y, K, K]);
-        cube.position.copy(new THREE.Vector3(2, 0, 1).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-
-        var cube = create_cube(size, [K, G, K, K, K, K]);
-        cube.position.copy(new THREE.Vector3(0, 1, 1).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.center_cubes.G = cube;
-        this.rubik_mesh.add(cube);
-        // var cube = create_cube(size, [K, K, K, K, K, K]);
-        // cube.position.copy(new THREE.Vector3(1, 1, 1).multiplyScalar(size).addScalar(size/2.0));
-        // cube_list.push(cube)
-        // this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [B, K, K, K, K, K]);
-        cube.position.copy(new THREE.Vector3(2, 1, 1).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube);
-        this.center_cubes.B = cube;
-        this.rubik_mesh.add(cube);
-
-        var cube = create_cube(size, [K, G, W, K, K, K]);
-        cube.position.copy(new THREE.Vector3(0, 2, 1).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [K, K, W, K, K, K]);
-        cube.position.copy(new THREE.Vector3(1, 2, 1).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.center_cubes.W = cube;
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [B, K, W, K, K, K]);
-        cube.position.copy(new THREE.Vector3(2, 2, 1).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube);
-        this.rubik_mesh.add(cube);
+        this.insertCube(1, 0, 1, [K, K, K, Y, K, K], 'Y');
+        this.insertCube(0, 0, 1, [K, G, K, Y, K, K]);
+        this.insertCube(2, 0, 1, [B, K, K, Y, K, K]);
+        this.insertCube(0, 1, 1, [K, G, K, K, K, K], 'G');
+        // this.insertCube(1, 1, 1, [K, K, K, K, K, K]);
+        this.insertCube(2, 1, 1, [B, K, K, K, K, K], 'B');
+        this.insertCube(0, 2, 1, [K, G, W, K, K, K]);
+        this.insertCube(1, 2, 1, [K, K, W, K, K, K], 'W');
+        this.insertCube(2, 2, 1, [B, K, W, K, K, K]);
 
 
         // Top
-        var cube = create_cube(size, [K, G, K, Y, R, K]);
-        cube.position.copy(new THREE.Vector3(0, 0, 2).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [K, K, K, Y, R, K]);
-        cube.position.copy(new THREE.Vector3(1, 0, 2).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [B, K, K, Y, R, K]);
-        cube.position.copy(new THREE.Vector3(2, 0, 2).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-
-        var cube = create_cube(size, [K, G, K, K, R, K]);
-        cube.position.copy(new THREE.Vector3(0, 1, 2).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [K, K, K, K, R, K]);
-        cube.position.copy(new THREE.Vector3(1, 1, 2).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.center_cubes.R = cube;
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [B, K, K, K, R, K]);
-        cube.position.copy(new THREE.Vector3(2, 1, 2).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-
-        var cube = create_cube(size, [K, G, W, K, R, K]);
-        cube.position.copy(new THREE.Vector3(0, 2, 2).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [K, K, W, K, R, K]);
-        cube.position.copy(new THREE.Vector3(1, 2, 2).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
-        var cube = create_cube(size, [B, K, W, K, R, K]);
-        cube.position.copy(new THREE.Vector3(2, 2, 2).multiplyScalar(size).addScalar(size/2.0));
-        cube_list.push(cube)
-        this.rubik_mesh.add(cube);
+        this.insertCube(0, 0, 2, [K, G, K, Y, R, K]);
+        this.insertCube(1, 0, 2, [K, K, K, Y, R, K]);
+        this.insertCube(2, 0, 2, [B, K, K, Y, R, K]);
+        this.insertCube(0, 1, 2, [K, G, K, K, R, K]);
+        this.insertCube(1, 1, 2, [K, K, K, K, R, K], 'R');
+        this.insertCube(2, 1, 2, [B, K, K, K, R, K]);
+        this.insertCube(0, 2, 2, [K, G, W, K, R, K]);
+        this.insertCube(1, 2, 2, [K, K, W, K, R, K]);
+        this.insertCube(2, 2, 2, [B, K, W, K, R, K]);
 
         this.pivot_mesh.add(this.rubik_mesh);
         this.rubik_mesh.position.setScalar(-size * 1.5);
 
-        this.list = cube_list
         this.curr_subgroup = null;
 
         // Assign name to center cubes
@@ -251,6 +143,31 @@ class Rubik{
         .repeatDelay(1000)
         .easing(TWEEN.Easing.Elastic.InOut)
         .start();
+    }
+
+    insertCube(x, y, z, mat_arr, center_cube){
+        // faces are front back up down left right
+        let c_size = this.size * this.scale
+        const geo = new THREE.BoxGeometry(c_size, c_size, c_size);
+        const cube = new THREE.Group();
+        const inner_cube = new THREE.Group();
+        const mesh = new THREE.Mesh( geo, mat_arr );
+        const edges = new THREE.EdgesGeometry( geo );
+        const line = new THREE.LineSegments( edges,  lineMat);
+        line.name = 'line';
+        inner_cube.add( line );
+        inner_cube.add( mesh );
+        cube.add(inner_cube);
+        
+        cube.position.copy(new THREE.Vector3(x, y, z).multiplyScalar(this.size).addScalar(this.size/2.0));
+
+        inner_cube.position.add(new THREE.Vector3(1 - x, 1 - y, 1 - z).multiplyScalar((1 - this.scale) * this.size * 0.99));
+        inner_cube.closedPos = inner_cube.position.clone();
+        this.list.push(cube);
+        if(center_cube){
+            this.center_cubes[center_cube] = cube;
+        }
+        this.rubik_mesh.add(cube);
     }
 
     getRubikSpacePos(in_cube){
@@ -338,14 +255,14 @@ var tweenRotateOnAxis = function() {
 
     var qStart = new THREE.Quaternion();
     var o = new THREE.Object3D();
-    return function tweenRotateOnAxis( object, axis, angle, rot_t, end_callback) {
+    return function tweenRotateOnAxis( object, axis, angle, rot_t, do_open_close, end_callback) {
 
         if(isRotating){
             return false;
         }
 
         isRotating = true;
-        var qEnd, time = { t: 0 };
+        var qEnd;
 
         // start quaternion
         qStart.copy( object.quaternion );
@@ -355,12 +272,36 @@ var tweenRotateOnAxis = function() {
         o.rotateOnAxis( axis, angle );
         qEnd = o.quaternion;
 
+        if(do_open_close){
+            let open_tween = new TWEEN.Tween({t: 1})
+                .to({t:0}, rot_t * 0.4)
+                .easing(TWEEN.Easing.Quadratic.Out)
+                .onUpdate( function(obj) {
+                    rubik.list.forEach((cube)=>{
+                        cube.children[0].position.copy(cube.children[0].closedPos);
+                        cube.children[0].position.multiplyScalar(obj.t);
+                    });
+                } );
+            let close_tween = new TWEEN.Tween({t: 0})
+                .to({t:1}, rot_t * 0.4)
+                .easing(TWEEN.Easing.Quadratic.Out)
+                .onUpdate( function(obj) {
+                    rubik.list.forEach((cube)=>{
+                        cube.children[0].position.copy(cube.children[0].closedPos);
+                        cube.children[0].position.multiplyScalar(obj.t);
+                    });
+                } )
+                .delay(rot_t * 0.2);
+            open_tween.chain(close_tween);
+            open_tween.start();
+        }
+
         // tween
-        new TWEEN.Tween( time )
+        new TWEEN.Tween( {t: 0} )
             .to( { t : 1 }, rot_t )
             .easing( TWEEN.Easing.Back.InOut )
-            .onUpdate( function() {
-                object.quaternion.slerpQuaternions( qStart, qEnd, time.t );
+            .onUpdate( function(obj) {
+                object.quaternion.slerpQuaternions( qStart, qEnd, obj.t );
             } )
             .onComplete( function() {
                 object.quaternion.copy( qEnd ); // to be exact
@@ -370,7 +311,6 @@ var tweenRotateOnAxis = function() {
                 }
             } )
             .start();
-
         return true;
     };
 
@@ -401,20 +341,42 @@ function getCartesianClosest(dir){
     return new THREE.Vector3(v[0], v[1], v[2]);
 }
 
-function commenceFaceRotation(ax, reverse, time, callback){
+function commenceFaceRotation(ax, reverse, time, do_open_close, callback){
     if(isRotating){
         return false;
     }
     let f = rubik.getFaceSubgroup(ax)
     let coeff = reverse ? -0.5 : 0.5; 
     if(f != null){
-        tweenRotateOnAxis(f, ax, coeff * Math.PI, time, ()=>{
+        if(do_open_close){
+            var open_tween = new TWEEN.Tween({t: 1})
+                .to({t:0}, time * 0.4)
+                .easing(TWEEN.Easing.Quadratic.Out)
+                .onUpdate( function(obj) {
+                    rubik.list.forEach((cube)=>{
+                        cube.children[0].position.copy(cube.children[0].closedPos);
+                        cube.children[0].position.multiplyScalar(obj.t);
+                    });
+                } );
+            let close_tween = new TWEEN.Tween({t: 0})
+                .to({t:1}, time * 0.4)
+                .easing(TWEEN.Easing.Quadratic.Out)
+                .onUpdate( function(obj) {
+                    rubik.list.forEach((cube)=>{
+                        cube.children[0].position.copy(cube.children[0].closedPos);
+                        cube.children[0].position.multiplyScalar(obj.t);
+                    });
+                } )
+                .delay(time * 0.2);
+            open_tween.chain(close_tween);
+        }
+        tweenRotateOnAxis(f, ax, coeff * Math.PI, time, do_open_close, ()=>{
             // Enforce cleanup
             rubik.cleanup()
             if(callback){
                 callback()
             }
-        })
+        });
     }
     return true;
 }
@@ -428,7 +390,7 @@ function getFacedRubikNormal(cam_dir){
 }
 
 
-const rubik = new Rubik(2);
+const rubik = new Rubik(2, 0.65);
 rubik.pivot_mesh.position.y = 5
 scene.add(rubik.pivot_mesh)
 
@@ -466,63 +428,63 @@ document.addEventListener('keydown', function(event) {
     var cam_up = new THREE.Vector3(0, 1, 0);
     cam_up.transformDirection(camera.matrixWorld)
 
-    time = 600
+    time = 800
 
     if (event.key === ',') {
-        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_dir), 0.5 * Math.PI, time)
+        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_dir), 0.5 * Math.PI, time, true)
     }
     if (event.key === '.') {
-        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_dir), -0.5 * Math.PI, time)
+        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_dir), -0.5 * Math.PI, time, true)
     }
     if (event.key === 'ArrowUp') {
-        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_dir.cross(cam_up)), 0.5 * Math.PI, time)
+        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_dir.cross(cam_up)), 0.5 * Math.PI, time, true)
     }
     if (event.key === 'ArrowDown') {
-        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_dir.cross(cam_up)), -0.5 * Math.PI, time)
+        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_dir.cross(cam_up)), -0.5 * Math.PI, time, true)
     }
     if (event.key === 'ArrowRight') {
-        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_up), -0.5 * Math.PI, time)
+        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_up), -0.5 * Math.PI, time, true)
     }
     if (event.key === 'ArrowLeft') {
-        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_up), 0.5 * Math.PI, time)
+        tweenRotateOnAxis(rubik.pivot_mesh, getFacedRubikNormal(cam_up), 0.5 * Math.PI, time, true)
     }
 
     if (event.key === 'f') {
-        commenceFaceRotation(getFacedRubikNormal(cam_dir), true, time)
+        commenceFaceRotation(getFacedRubikNormal(cam_dir), true, time, true)
     }
     if (event.key === 'r') {
-        commenceFaceRotation(getFacedRubikNormal(cam_dir.cross(cam_up).multiplyScalar(-1)), true, time)
+        commenceFaceRotation(getFacedRubikNormal(cam_dir.cross(cam_up).multiplyScalar(-1)), true, time, true)
     }
     if (event.key === 'u') {
-        commenceFaceRotation(getFacedRubikNormal(cam_up.multiplyScalar(-1)), true, time)    
+        commenceFaceRotation(getFacedRubikNormal(cam_up.multiplyScalar(-1)), true, time, true)    
     }
     if (event.key === 'l') {
-        commenceFaceRotation(getFacedRubikNormal(cam_dir.cross(cam_up)), true, time)    
+        commenceFaceRotation(getFacedRubikNormal(cam_dir.cross(cam_up)), true, time, true)    
     }
     if (event.key === 'b') {
-        commenceFaceRotation(getFacedRubikNormal(cam_dir.multiplyScalar(-1)), true, time)
+        commenceFaceRotation(getFacedRubikNormal(cam_dir.multiplyScalar(-1)), true, time, true) 
     }
     if (event.key === 'd') {
-        commenceFaceRotation(getFacedRubikNormal(cam_up), true, time)    
+        commenceFaceRotation(getFacedRubikNormal(cam_up), true, time, true) 
     }
 
     if (event.key === 'F') {
-        commenceFaceRotation(getFacedRubikNormal(cam_dir), false, time)
+        commenceFaceRotation(getFacedRubikNormal(cam_dir), false, time, true) 
     }
     if (event.key === 'R') {
-        commenceFaceRotation(getFacedRubikNormal(cam_dir.cross(cam_up).multiplyScalar(-1)), false, time)
+        commenceFaceRotation(getFacedRubikNormal(cam_dir.cross(cam_up).multiplyScalar(-1)), false, time, true) 
     }
     if (event.key === 'U') {
-        commenceFaceRotation(getFacedRubikNormal(cam_up.multiplyScalar(-1)), false, time)    
+        commenceFaceRotation(getFacedRubikNormal(cam_up.multiplyScalar(-1)), false, time, true)   
     }
     if (event.key === 'L') {
-        commenceFaceRotation(getFacedRubikNormal(cam_dir.cross(cam_up)), false, time)    
+        commenceFaceRotation(getFacedRubikNormal(cam_dir.cross(cam_up)), false, time, true)    
     }
     if (event.key === 'B') {
-        commenceFaceRotation(getFacedRubikNormal(cam_dir.multiplyScalar(-1)), false, time)
+        commenceFaceRotation(getFacedRubikNormal(cam_dir.multiplyScalar(-1)), false, time, true) 
     }
     if (event.key === 'D') {
-        commenceFaceRotation(getFacedRubikNormal(cam_up), false, time)    
+        commenceFaceRotation(getFacedRubikNormal(cam_up), false, time, true)    
     }
 
     if (event.key === ' ') {
@@ -550,7 +512,7 @@ function shuffle(iters, time, callback){
 
     // Get random rotation
     let rev = (Math.floor(Math.random() * 2) == 1);
-    return commenceFaceRotation(normal, rev, time, ()=>{
+    return commenceFaceRotation(normal, rev, time, false, ()=>{
         if(iters == 0){
             callback();
             return;
